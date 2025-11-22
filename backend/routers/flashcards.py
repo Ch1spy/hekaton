37ebeb0039e503bb43890556148d2c2c5eccd6fe
Ch1spy <chiisppy@gmail.com>
2025-11-22 22:35:24 +0100

    flashcards

---
+++ backend/routers/flashcards.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, HTTPException
+from backend.db import supabase
+
+router = APIRouter(
+    prefix="/flashcards",
+    tags=["Flashcards"]
+)
+
+@router.get("/")
+def get_all_flashcards():
+    response = supabase.table("flashcards").select("*").execute()
+    return response.data
+
+
+@router.get("/{card_id}")
+def get_flashcard(card_id: int):
+    response = (
+        supabase.table("flashcards")
+        .select("*")
+        .eq("id", card_id)
+        .single()
+        .execute()
+    )
+
+    if not response.data:
+        raise HTTPException(status_code=404, detail="Flashcard not found")
+
+    return response.data
